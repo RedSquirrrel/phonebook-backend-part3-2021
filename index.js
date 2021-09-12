4bac80a72129b092cde7a55a9927204ce99d2c4c
RedSquirrrel <anet1@rocketmail.com>
2021-09-12 16:56:47 +0100

    removed console.log

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
   }
-  console.log(error);
   next(error);
 };
 
